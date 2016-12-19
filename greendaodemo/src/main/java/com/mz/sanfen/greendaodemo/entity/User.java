44c95d96d94f97051d8bb5b255c2b02abcf9c35a
sanfen <sanfen@sanfendeMacBook-Pro.local>
2016-12-19 09:30:21 +0800

    添加外键

--- greendaodemo/src/main/java/com/mz/sanfen/greendaodemo/entity/User.java
+++ greendaodemo/src/main/java/com/mz/sanfen/greendaodemo/entity/User.java
@@ -3,6 +3,11 @@ package com.mz.sanfen.greendaodemo.entity;
 import org.greenrobot.greendao.annotation.Entity;
 import org.greenrobot.greendao.annotation.Id;
 import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.annotation.Index;
+import org.greenrobot.greendao.annotation.NotNull;
+import org.greenrobot.greendao.annotation.Property;
+import org.greenrobot.greendao.annotation.ToOne;
+import org.greenrobot.greendao.DaoException;
 
 /**
  * @author MZ
@@ -10,22 +15,48 @@ import org.greenrobot.greendao.annotation.Generated;
  * @date 16/12/15.
  */
 
-@Entity
+@Entity(
+        active = true,
+        nameInDb = "USER",
+        createInDb = false)
 public class User {
 
-    @Id
+    @Id(autoincrement = true)
     private Long id;
+
+    @NotNull
     private String name;
+
     private int age;
-    @Generated(hash = 1309193360)
-    public User(Long id, String name, int age) {
+
+    private Long blogId;
+
+    @ToOne(joinProperty = "blogId")
+    private Blog blog;
+
+    /** Used to resolve relations */
+    @Generated(hash = 2040040024)
+    private transient DaoSession daoSession;
+
+    /** Used for active entity operations. */
+    @Generated(hash = 1507654846)
+    private transient UserDao myDao;
+
+    @Generated(hash = 384172391)
+    private transient Long blog__resolvedKey;
+
+    @Generated(hash = 1535784894)
+    public User(Long id, @NotNull String name, int age, Long blogId) {
         this.id = id;
         this.name = name;
         this.age = age;
+        this.blogId = blogId;
     }
+
     @Generated(hash = 586692638)
     public User() {
     }
+
     public Long getId() {
         return this.id;
     }
@@ -43,5 +74,85 @@ public class User {
     }
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public Long getBlogId() {
+        return this.blogId;
+    }
+
+    public void setBlogId(Long blogId) {
+        this.blogId = blogId;
+    }
+
+    /** To-one relationship, resolved on first access. */
+    @Generated(hash = 1907406751)
+    public Blog getBlog() {
+        Long __key = this.blogId;
+        if (blog__resolvedKey == null || !blog__resolvedKey.equals(__key)) {
+            final DaoSession daoSession = this.daoSession;
+            if (daoSession == null) {
+                throw new DaoException("Entity is detached from DAO context");
+            }
+            BlogDao targetDao = daoSession.getBlogDao();
+            Blog blogNew = targetDao.load(__key);
+            synchronized (this) {
+                blog = blogNew;
+                blog__resolvedKey = __key;
+            }
+        }
+        return blog;
+    }
+
+    /** called by internal mechanisms, do not call yourself. */
+    @Generated(hash = 1063134684)
+    public void setBlog(Blog blog) {
+        synchronized (this) {
+            this.blog = blog;
+            blogId = blog == null ? null : blog.getId();
+            blog__resolvedKey = blogId;
+        }
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#delete(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 128553479)
+    public void delete() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.delete(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#refresh(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 1942392019)
+    public void refresh() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.refresh(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#update(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 713229351)
+    public void update() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.update(this);
+    }
+
+    /** called by internal mechanisms, do not call yourself. */
+    @Generated(hash = 2059241980)
+    public void __setDaoSession(DaoSession daoSession) {
+        this.daoSession = daoSession;
+        myDao = daoSession != null ? daoSession.getUserDao() : null;
     }
 }
--- greendaodemo/src/main/java/com/mz/sanfen/greendaodemo/entity/UserDao.java
+++ greendaodemo/src/main/java/com/mz/sanfen/greendaodemo/entity/UserDao.java
@@ -1,10 +1,13 @@
 package com.mz.sanfen.greendaodemo.entity;
 
+import java.util.List;
+import java.util.ArrayList;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteStatement;
 
 import org.greenrobot.greendao.AbstractDao;
 import org.greenrobot.greendao.Property;
+import org.greenrobot.greendao.internal.SqlUtils;
 import org.greenrobot.greendao.internal.DaoConfig;
 import org.greenrobot.greendao.database.Database;
 import org.greenrobot.greendao.database.DatabaseStatement;
@@ -25,7 +28,10 @@ public class UserDao extends AbstractDao<User, Long> {
         public final static Property Id = new Property(0, Long.class, "id", true, "_id");
         public final static Property Name = new Property(1, String.class, "name", false, "NAME");
         public final static Property Age = new Property(2, int.class, "age", false, "AGE");
+        public final static Property BlogId = new Property(3, Long.class, "blogId", false, "BLOG_ID");
     }
+
+    private DaoSession daoSession;
 
 
     public UserDao(DaoConfig config) {
@@ -34,21 +40,7 @@ public class UserDao extends AbstractDao<User, Long> {
     
     public UserDao(DaoConfig config, DaoSession daoSession) {
         super(config, daoSession);
-    }
-
-    /** Creates the underlying database table. */
-    public static void createTable(Database db, boolean ifNotExists) {
-        String constraint = ifNotExists? "IF NOT EXISTS ": "";
-        db.execSQL("CREATE TABLE " + constraint + "\"USER\" (" + //
-                "\"_id\" INTEGER PRIMARY KEY ," + // 0: id
-                "\"NAME\" TEXT," + // 1: name
-                "\"AGE\" INTEGER NOT NULL );"); // 2: age
-    }
-
-    /** Drops the underlying database table. */
-    public static void dropTable(Database db, boolean ifExists) {
-        String sql = "DROP TABLE " + (ifExists ? "IF EXISTS " : "") + "\"USER\"";
-        db.execSQL(sql);
+        this.daoSession = daoSession;
     }
 
     @Override
@@ -59,12 +51,13 @@ public class UserDao extends AbstractDao<User, Long> {
         if (id != null) {
             stmt.bindLong(1, id);
         }
- 
-        String name = entity.getName();
-        if (name != null) {
-            stmt.bindString(2, name);
-        }
+        stmt.bindString(2, entity.getName());
         stmt.bindLong(3, entity.getAge());
+ 
+        Long blogId = entity.getBlogId();
+        if (blogId != null) {
+            stmt.bindLong(4, blogId);
+        }
     }
 
     @Override
@@ -75,12 +68,19 @@ public class UserDao extends AbstractDao<User, Long> {
         if (id != null) {
             stmt.bindLong(1, id);
         }
- 
-        String name = entity.getName();
-        if (name != null) {
-            stmt.bindString(2, name);
-        }
+        stmt.bindString(2, entity.getName());
         stmt.bindLong(3, entity.getAge());
+ 
+        Long blogId = entity.getBlogId();
+        if (blogId != null) {
+            stmt.bindLong(4, blogId);
+        }
+    }
+
+    @Override
+    protected final void attachEntity(User entity) {
+        super.attachEntity(entity);
+        entity.__setDaoSession(daoSession);
     }
 
     @Override
@@ -92,8 +92,9 @@ public class UserDao extends AbstractDao<User, Long> {
     public User readEntity(Cursor cursor, int offset) {
         User entity = new User( //
             cursor.isNull(offset + 0) ? null : cursor.getLong(offset + 0), // id
-            cursor.isNull(offset + 1) ? null : cursor.getString(offset + 1), // name
-            cursor.getInt(offset + 2) // age
+            cursor.getString(offset + 1), // name
+            cursor.getInt(offset + 2), // age
+            cursor.isNull(offset + 3) ? null : cursor.getLong(offset + 3) // blogId
         );
         return entity;
     }
@@ -101,8 +102,9 @@ public class UserDao extends AbstractDao<User, Long> {
     @Override
     public void readEntity(Cursor cursor, User entity, int offset) {
         entity.setId(cursor.isNull(offset + 0) ? null : cursor.getLong(offset + 0));
-        entity.setName(cursor.isNull(offset + 1) ? null : cursor.getString(offset + 1));
+        entity.setName(cursor.getString(offset + 1));
         entity.setAge(cursor.getInt(offset + 2));
+        entity.setBlogId(cursor.isNull(offset + 3) ? null : cursor.getLong(offset + 3));
      }
     
     @Override
@@ -130,4 +132,95 @@ public class UserDao extends AbstractDao<User, Long> {
         return true;
     }
     
+    private String selectDeep;
+
+    protected String getSelectDeep() {
+        if (selectDeep == null) {
+            StringBuilder builder = new StringBuilder("SELECT ");
+            SqlUtils.appendColumns(builder, "T", getAllColumns());
+            builder.append(',');
+            SqlUtils.appendColumns(builder, "T0", daoSession.getBlogDao().getAllColumns());
+            builder.append(" FROM USER T");
+            builder.append(" LEFT JOIN BLOG T0 ON T.\"BLOG_ID\"=T0.\"_id\"");
+            builder.append(' ');
+            selectDeep = builder.toString();
+        }
+        return selectDeep;
+    }
+    
+    protected User loadCurrentDeep(Cursor cursor, boolean lock) {
+        User entity = loadCurrent(cursor, 0, lock);
+        int offset = getAllColumns().length;
+
+        Blog blog = loadCurrentOther(daoSession.getBlogDao(), cursor, offset);
+        entity.setBlog(blog);
+
+        return entity;    
+    }
+
+    public User loadDeep(Long key) {
+        assertSinglePk();
+        if (key == null) {
+            return null;
+        }
+
+        StringBuilder builder = new StringBuilder(getSelectDeep());
+        builder.append("WHERE ");
+        SqlUtils.appendColumnsEqValue(builder, "T", getPkColumns());
+        String sql = builder.toString();
+        
+        String[] keyArray = new String[] { key.toString() };
+        Cursor cursor = db.rawQuery(sql, keyArray);
+        
+        try {
+            boolean available = cursor.moveToFirst();
+            if (!available) {
+                return null;
+            } else if (!cursor.isLast()) {
+                throw new IllegalStateException("Expected unique result, but count was " + cursor.getCount());
+            }
+            return loadCurrentDeep(cursor, true);
+        } finally {
+            cursor.close();
+        }
+    }
+    
+    /** Reads all available rows from the given cursor and returns a list of new ImageTO objects. */
+    public List<User> loadAllDeepFromCursor(Cursor cursor) {
+        int count = cursor.getCount();
+        List<User> list = new ArrayList<User>(count);
+        
+        if (cursor.moveToFirst()) {
+            if (identityScope != null) {
+                identityScope.lock();
+                identityScope.reserveRoom(count);
+            }
+            try {
+                do {
+                    list.add(loadCurrentDeep(cursor, false));
+                } while (cursor.moveToNext());
+            } finally {
+                if (identityScope != null) {
+                    identityScope.unlock();
+                }
+            }
+        }
+        return list;
+    }
+    
+    protected List<User> loadDeepAllAndCloseCursor(Cursor cursor) {
+        try {
+            return loadAllDeepFromCursor(cursor);
+        } finally {
+            cursor.close();
+        }
+    }
+    
+
+    /** A raw-style query where you can pass any WHERE clause and arguments. */
+    public List<User> queryDeep(String where, String... selectionArg) {
+        Cursor cursor = db.rawQuery(getSelectDeep() + where, selectionArg);
+        return loadDeepAllAndCloseCursor(cursor);
+    }
+ 
 }
